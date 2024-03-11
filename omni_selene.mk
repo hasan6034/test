e324df326255b4a204b80e7205940590a0847a73
Hasan Basri OY <hasanbasrioy@gmail.com>
2024-03-11 09:30:48 +0300

    a

---
+++ omni_selene.mk
@@ -0,0 +1,18 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit some common pbrp stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
+
+# Inherit from selene device
+$(call inherit-product, device/xiaomi/selene/device.mk)
+
+PRODUCT_DEVICE := selene
+PRODUCT_NAME := omni_selene
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 10
+PRODUCT_MANUFACTURER := Xiaomi
