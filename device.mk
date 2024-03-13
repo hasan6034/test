56312e9f81e81109ae5308eb4cefae9b0a79a956
Hasan Basri OY <hasanbasrioy@gmail.com>
2024-03-13 12:03:19 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -56,7 +56,10 @@ PRODUCT_PACKAGES += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-mtkimpl.recovery
+    android.hardware.boot@1.2-impl-recovery \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-service \
+    bootctrl.$(PRODUCT_PLATFORM).recovery
 
 # Vibrator modules
 TARGET_RECOVERY_DEVICE_MODULES += \
